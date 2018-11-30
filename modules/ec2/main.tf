c769741d39f5e7a1ea2225a67475efb57f388dfd
detotam <roman.zhulia2009@gmail.com>
2018-11-30 11:58:33 +0200

    working code-winrm

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -22,32 +22,9 @@ resource "aws_instance" "ec2-instance" {
 <powershell>
 net user ${var.instance_username} '${var.admin_password}' /add /y
 net localgroup administrators ${var.instance_username} /add
-
-winrm quickconfig -q
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB=”300″}'
-winrm set winrm/config '@{MaxTimeoutms="1800000"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
-
-netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
-netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
-
-net stop winrm
-sc.exe config winrm start=auto
-net start winrm
 </powershell>
 EOF
-
-  provisioner "file" {
-  source = "test.txt"
-  destination = "C:/test.txt"
-    connection {
-      type = "winrm"
-      timeout = "10m"
-      user = "${var.instance_username}"
-      password = "${var.admin_password}"
-    }
-  }
+  
 }
 
 output "aws_instance_id" {
